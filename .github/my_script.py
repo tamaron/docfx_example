70cc6bcca0ca799c5c74cf83c25e10f7b865ee32
tamaron <49875900+tamaron@users.noreply.github.com>
2022-09-17 00:14:11 +0900

    Update my_script.py

--- .github/my_script.py
+++ .github/my_script.py
@@ -1,1 +1,24 @@
-print('hello World') 
+import os
+import glob
+from xml.etree.ElementInclude import include
+
+os.chdir('../')
+os.chdir('_site/api')
+path = os.getcwd()
+
+files = glob.glob("./*")
+for fileName in files:
+    with open(fileName ,"r",encoding="utf-8") as file:
+        lines=file.read()
+
+    if 'SampleNamespace.TypeA' in lines:
+        print(fileName)
+        lines = lines.replace('cs.temp.dll.dll', 'SampleNamespace.TypeA.dll')
+    elif 'SampleNamespace.TypeB' in lines:
+        lines = lines.replace('cs.temp.dll.dll', 'SampleNamespace.TypeB.dll')
+    
+
+    print(lines)
+    with open(fileName ,"w",encoding="utf-8") as file:
+        file.write(lines)
+    
